8570aa2287058488d45212add62a7832335408a3
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-31 07:58:57 +0000

    commit

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link, useParams } from "react-router-dom";
 
-const Card = () => {
+
+function Card() {
+    const { store, actions } = useContext(Context);
+    const { id } = useParams();
+
+    const handleDeleteClick = (contact) => {
+        actions.setContactToBeDeleted(contact);
+        actions.toggleModal(true);
+    };
+
     return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div classNameName="card-body">
-                <h5 classNameName="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
+        <div>
+            {store.contacts.map((contact) =>
+                <div className="card mb-3 mx-5" key={contact.id} >
+                    <div className="row g-0">
+                        <div className="col-md-3">
+                            <img src={avatar} className="img-fluid" alt="Contact Profile Picture" />
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card-body">
+                                <h2 className="card-title">{contact.name}</h2>
+                                <div className="profile-details d-flex">
+                                    <i className="fa fa-map-marker-alt" />
+                                    <p className="card-text">{contact.homeAddress}</p>
+                                </div>
+                                <div className="profile-details d-flex">
+                                    <i className="fas fa-phone" />
+                                    <p className="card-text">
+                                        <small>{contact.phone}</small>
+                                    </p>
+                                </div>
+                                <div className="profile-details d-flex">
+                                    <i className="fas fa-at" />
+                                    <p className="card-text">
+                                        <small>{contact.email}</small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <Link to={`/edit/${contact.id}`}>
+                                <button className="card-button" >
+                                    <i className="fas fa-pencil-alt" />
+                                </button>
+                            </Link>
+                            <button className="card-button" onClick={() => handleDeleteClick(contact)}>
+                                <i className="fas fa-trash-alt" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <DeleteModal />
         </div>
-    )
+    );
 }
 
-export default Card
+
+export default Card;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,105 +1,113 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			data() {
-				return {
-				  showModal: false,
-				  contactToBeDeleted: null,
-				  contacts: [
-					{
-					  id: 1,
-					  name: "Vincent van Gogh",
-					  homeAddress: "123 Main St.",
-					  phone: "(555) 123-4567",
-					  email: "vincent@example.com"
-					},
-					{
-					  id: 2,
-					  name: "Leonardo da Vinci",
-					  homeAddress: "456 Oak Ave.",
-					  phone: "(444) 987-6543",
-					  email: "leonardo@example.com"
-					},
-					{
-					  id: 3,
-					  name: "Pablo Picasso",
-					  homeAddress: "789 Maple Rd.",
-					  phone: "(777) 321-5555",
-					  email: "pablo@example.com"
-					},
-					{
-					  id: 4,
-					  name: "Frida Kahlo",
-					  homeAddress: "567 Pine St.",
-					  phone: "(222) 555-8888",
-					  email: "frida@example.com"
-					},
-				  ]
-				}
-			  },
-			  
-			 
-				  actions: {
-					// Add contact to the list
-					addContact: (contact) => {
-					  let listOfContacts = getStore().contacts;
-					  const newContact = {
-						id: listOfContacts.length + 1,
-						...contact
-					  };
-					  setStore({ contacts: [...listOfContacts, newContact] });
-					},
-			  
-					// Edit contact from the list
-					editContact: (id, updatedContact) => {
-					  let listOfContacts = getStore().contacts;
-					  const contactIndex = listOfContacts.findIndex(contact => contact.id === id);
-					  if (contactIndex !== -1) {
-						const updatedContacts = [...listOfContacts];
-						updatedContacts[contactIndex] = { id, ...updatedContact };
-						setStore({ contacts: updatedContacts });
-					  }
-					},
-			  
-					// Toggle the modal on
-					toggleModal: (show) => {
-					  setStore({ showModal: show })
-					},
-			  
-					// Check if all fields are filled else show modal
-					checkEmptyFields: (newContact) => {
-					  const { name, homeAddress, phone, email } = newContact;
-					  if (name && homeAddress && phone && email) {
-						getActions().addContact(newContact);
-					  } else {
-						getActions().toggleModal(true);
-					  }
-					},
-			  
-					// Close modal button
-					closeModal: () => {
-					  setStore({ showModal: false });
-					},
-			  
-					// Set contact to be deleted
-					setContactToBeDeleted: (contact) => {
-					  setStore({ contactToBeDeleted: contact });
-					},
-			  
-					// Close the confirm delete modal without deleting a record
-					closeDeleteModal: () => {
-					  setStore({ showModal: false, contactToBeDeleted: null });
-					},
-			  
-					// Delete contact from the list
-					deleteContact: (contact) => {
-					  let listOfContacts = getStore().contacts;
-					  setStore({ contacts: listOfContacts.filter((item) => item !== contact) });
-					  getActions().closeDeleteModal();
-					},
-				  }
+
+			showModal: false,
+			contactToBeDeleted: null,
+			contacts: [
+				{
+					id: 1,
+					name: "Leonardo da Vinci",
+					homeAddress: "Florence, Italy",
+					phone: "(555) 123-4567",
+					email: "leonardo@exampleemail.com"
+				},
+				{
+					id: 2,
+					name: "Vincent van Gogh",
+					homeAddress: "Amsterdam, Netherlands",
+					phone: "(555) 987-6543",
+					email: "vincent@exampleemail.com"
+				},
+				{
+					id: 3,
+					name: "Pablo Picasso",
+					homeAddress: "Malaga, Spain",
+					phone: "(555) 555-5555",
+					email: "pablo@exampleemail.com"
+				},
+				{
+					id: 4,
+					name: "Frida Kahlo",
+					homeAddress: "Mexico City, Mexico",
+					phone: "(555) 111-2222",
+					email: "frida@exampleemail.com"
+				},
+			]
+		},
+
+		actions: {
+
+			//add contact to the list
+			addContact: (contact) => {
+				// pulls contacts from the store
+				let listOfContacts = getStore().contacts;
+				// creates an id for the new contact and add rest of its info
+				const newContact = {
+					id: listOfContacts.length + 1,
+					...contact
 				};
-			  };
-			  
-			  export default getState;
-			  
+				// adds new contact to the list
+				setStore({ contacts: [...listOfContacts, newContact] });
+			},
+
+			//edit contact from the list
+			editContact: (id, updatedContact) => {
+				// pulls contacts from the store
+				let listOfContacts = getStore().contacts;
+				// if contact with id exists then update it
+				const contactIndex = listOfContacts.findIndex(contact => contact.id === id);
+				if (contactIndex !== -1) {
+					const updatedContacts = [...listOfContacts];
+					updatedContacts[contactIndex] = { id, ...updatedContact };
+					// updates the contacts' info in the store
+					setStore({ contacts: updatedContacts });
+				}
+			},
+
+			// toggle the modal on
+			toggleModal: (show) => {
+				setStore({ showModal: show })
+			},
+
+			// checks if all fields are filled else shows modals
+			checkEmptyFields: (newContact) => {
+				const { name, homeAddress, phone, email } = newContact;
+				if (name && homeAddress && phone && email) {
+					// if all fields are filled, saves contact
+					getActions().addContact(newContact);
+				} else {
+					// if any field is empty, shows modal
+					getActions().toggleModal(true);
+				}
+			},
+
+			//close modal button
+			closeModal: () => {
+				setStore({ showModal: false });
+			},
+
+			// set contact to be deleted
+			setContactToBeDeleted: (contact) => {
+				setStore({ contactToBeDeleted: contact });
+			},
+
+			// close the confirm delete modal without deleting a record
+			closeDeleteModal: () => {
+				setStore({ showModal: false, contactToBeDeleted: null });
+			},
+
+			//delete contact from the list
+			deleteContact: (contact) => {
+				//pulls the contacts in the store
+				let listOfContacts = getStore().contacts;
+				//filters the contact and generates new array
+				setStore({ contacts: listOfContacts.filter((item) => item !== contact) });
+				// close the confirm delete modal after deleting a record
+				getActions().closeDeleteModal();
+			},
+		}
+	};
+};
+
+export default getState;
