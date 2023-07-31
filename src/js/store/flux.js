0bf183216a5daa5a9bf6c9daa6b7be6ba269638d
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-31 06:05:01 +0000

    commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,47 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			data() {
+				return {
+				  showModal: false,
+				  contactToBeDeleted: null,
+				  contacts: [
+					{
+					  id: 1,
+					  name: "Vincent van Gogh",
+					  homeAddress: "123 Main St.",
+					  phone: "(555) 123-4567",
+					  email: "vincent@example.com"
+					},
+					{
+					  id: 2,
+					  name: "Leonardo da Vinci",
+					  homeAddress: "456 Oak Ave.",
+					  phone: "(444) 987-6543",
+					  email: "leonardo@example.com"
+					},
+					{
+					  id: 3,
+					  name: "Pablo Picasso",
+					  homeAddress: "789 Maple Rd.",
+					  phone: "(777) 321-5555",
+					  email: "pablo@example.com"
+					},
+					{
+					  id: 4,
+					  name: "Frida Kahlo",
+					  homeAddress: "567 Pine St.",
+					  phone: "(222) 555-8888",
+					  email: "frida@example.com"
+					},
+				  ]
+				}
+			  },
+			  
+			  actions: {
+				//add contact to
+			  }
+			  
 			demo: [
 				{
 					title: "FIRST",
