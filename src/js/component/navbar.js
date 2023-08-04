734500e72c4ff8da6efbeb8192fdc77f7a395eb8
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-04 23:05:12 +0000

    commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,5 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1"></span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary"></button>
-				</Link>
-			</div>
-		</nav>
-	);
+	
 };
--- src/js/views/edit.js
+++ src/js/views/edit.js
@@ -3,23 +3,64 @@ import "../../styles/home.css";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-
 const Edit = () => {
-  const params = useParams()
-  const { store } = useContext(Context)
-  const [detail, setDetail] = useState({})
+  const params = useParams();
+  const { store, actions } = useContext(Context);
+  const [detail, setDetail] = useState({});
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
   function getDetail() {
-    const contact = store.contacts.filter((item) => item.id == params.id)
-    setDetail(contact)
+    const contact = store.contacts.find((item) => item.id === parseInt(params.id));
+    setDetail(contact);
   }
-  useEffect(() => { getDetail() }, [])
+
+  // Función para manejar los cambios en los campos del formulario
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetail((prevDetail) => ({
+      ...prevDetail,
+      [name]: value,
+    }));
+  };
+
+  // Función para enviar los cambios y actualizar el contacto
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    actions.editContact(detail.id, detail);
+    // Redireccionar a la página principal después de editar
+    // Aquí, reemplaza "/home" con la ruta de tu página principal
+    // o el componente que muestra la lista de contactos.
+    window.location.href = "/home";
+  };
+
   return (
     <div className="text-center mt-5">
-      estoy en la edicion del contacto  {params.id}
-      <Link to="/">ir al home</Link>
-      {detail.name}
-
-    </div>)
+      <h2>Editar contacto {params.id}</h2>
+      <Link to="/">Ir al home</Link>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nombre:</label>
+          <input type="text" name="name" value={detail.name || ''} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Dirección:</label>
+          <input type="text" name="homeAddress" value={detail.homeAddress || ''} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Teléfono:</label>
+          <input type="text" name="phone" value={detail.phone || ''} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={detail.email || ''} onChange={handleChange} />
+        </div>
+        <button type="submit">Guardar cambios</button>
+      </form>
+    </div>
+  );
 };
 
-export default Edit 
+export default Edit;
