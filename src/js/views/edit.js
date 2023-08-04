16e7523d4de57c5412e3a336310a9f4fd44d5581
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-04 21:54:25 +0000

    commit

--- src/js/views/edit.js
+++ src/js/views/edit.js
@@ -1,17 +1,25 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect, useState } from "react";
 import "../../styles/home.css";
 import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 
 const Edit = () => {
-    const params = useParams()
+  const params = useParams()
+  const { store } = useContext(Context)
+  const [detail, setDetail] = useState({})
+  function getDetail() {
+    const contact = store.contacts.filter((item) => item.id == params.id)
+    setDetail(contact)
+  }
+  useEffect(() => { getDetail() }, [])
   return (
-	<div className="text-center mt-5">
-        estoy en la edicion del contacto  {params.id}
-        <Link to="/">volver al home</Link>
+    <div className="text-center mt-5">
+      estoy en la edicion del contacto  {params.id}
+      <Link to="/">ir al home</Link>
+      {detail.name}
 
-	</div>)
+    </div>)
 };
 
 export default Edit 
