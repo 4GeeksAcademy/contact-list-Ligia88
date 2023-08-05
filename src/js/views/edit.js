874a23499ad8e1edd44a3c9f084321c31fd893d6
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-08-05 23:55:47 +0000

    commit

--- src/js/views/edit.js
+++ src/js/views/edit.js
@@ -1,63 +1,101 @@
-import React, { useContext, useEffect, useState } from "react";
-import "../../styles/home.css";
-import { Link, useParams } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 const Edit = () => {
-  const params = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
   const { store, actions } = useContext(Context);
-  const [detail, setDetail] = useState({});
+
+  const [updatedContact, setUpdatedContact] = useState({
+    name: "",
+    homeAddress: "",
+    phone: "",
+    email: "",
+  });
 
   useEffect(() => {
-    getDetail();
-  }, []);
+    // Carga los datos del contacto existente al montar el componente
+    const contact = store.contacts.find((item) => item.id === parseInt(id));
+    if (contact) {
+      setUpdatedContact(contact);
+    }
+  }, [id, store.contacts]);
 
-  function getDetail() {
-    const contact = store.contacts.find((item) => item.id === parseInt(params.id));
-    setDetail(contact);
-  }
-
-  // Función para manejar los cambios en los campos del formulario
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDetail((prevDetail) => ({
-      ...prevDetail,
-      [name]: value,
-    }));
-  };
-
-  // Función para enviar los cambios y actualizar el contacto
   const handleSubmit = (e) => {
     e.preventDefault();
-    actions.editContact(detail.id, detail);
-    // Redireccionar a la página principal después de editar
-    // Aquí, reemplaza "/home" con la ruta de tu página principal
-    // o el componente que muestra la lista de contactos.
-    window.location.href = "/home";
+    actions.editContact(parseInt(id), updatedContact);
+    // Redirige de regreso a la lista de contactos después de editar
+    navigate("/");
   };
 
   return (
-    <div className="text-center mt-5">
-      <h2>Editar contacto {params.id}</h2>
-      <Link to="/">Ir al home</Link>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre:</label>
-          <input type="text" name="name" value={detail.name || ''} onChange={handleChange} />
+    <div className="container my-3">
+      <form className="p-3" onSubmit={handleSubmit}>
+        <h1 className="text-center mb-4">Edit a Contact</h1>
+        <div className="form-group mb-3">
+          <label htmlFor="inputName">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            placeholder="Full Name"
+            value={updatedContact.name}
+            onChange={(e) =>
+              setUpdatedContact({ ...updatedContact, name: e.target.value })
+            }
+          />
         </div>
-        <div>
-          <label>Dirección:</label>
-          <input type="text" name="homeAddress" value={detail.homeAddress || ''} onChange={handleChange} />
+        <div className="form-group mb-3">
+          <label htmlFor="inputEmail">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="Enter Email"
+            value={updatedContact.email}
+            onChange={(e) =>
+              setUpdatedContact({ ...updatedContact, email: e.target.value })
+            }
+          />
         </div>
-        <div>
-          <label>Teléfono:</label>
-          <input type="text" name="phone" value={detail.phone || ''} onChange={handleChange} />
+        <div className="form-group mb-3">
+          <label htmlFor="inputPhone">Phone</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputPhone"
+            placeholder="Enter Phone"
+            value={updatedContact.phone}
+            onChange={(e) =>
+              setUpdatedContact({ ...updatedContact, phone: e.target.value })
+            }
+          />
         </div>
-        <div>
-          <label>Email:</label>
-          <input type="text" name="email" value={detail.email || ''} onChange={handleChange} />
+        <div className="form-group mb-3">
+          <label htmlFor="inputAddress">Address</label>
+          <textarea
+            className="form-control"
+            id="inputAddress"
+            rows="1"
+            placeholder="Enter Address"
+            value={updatedContact.homeAddress}
+            onChange={(e) =>
+              setUpdatedContact({
+                ...updatedContact,
+                homeAddress: e.target.value,
+              })
+            }
+          ></textarea>
         </div>
-        <button type="submit">Guardar cambios</button>
+        <div className="row text-center">
+          <button type="submit" className="btn btn-success w-50 mx-auto mt-4">
+            Save
+          </button>
+          <Link to="/">
+            <small>or get back to contacts</small>
+          </Link>
+        </div>
       </form>
     </div>
   );
